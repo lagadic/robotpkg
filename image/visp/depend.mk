bad9d10f89719cab14f99f45e7b6e284a66de418
Joudy Nader <Joudy.Nader@irisa.fr>
2020-03-11 15:43:34 +0100

    [image/visp] Update to 3.3.0
    Update to ViSP 3.3.0 release

--- image/visp/depend.mk
+++ image/visp/depend.mk
@@ -1,5 +1,5 @@
 # robotpkg depend.mk for:	image/visp
-# Created:			Anthony Mallet on Tue, 9 Feb 2010
+# Created:			10 March 2020
 #
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 VISP_DEPEND_MK:=	${VISP_DEPEND_MK}+
@@ -10,17 +10,17 @@ endif
 
 ifeq (+,$(VISP_DEPEND_MK)) # -----------------------------------------------
 
-PREFER.visp?=		robotpkg
+PREFER.visp?=	  robotpkg
 
 DEPEND_USE+=		visp
 
-DEPEND_ABI.visp?=	visp>=2.4.4
+DEPEND_ABI.visp?=	visp>=3.3.0
 DEPEND_DIR.visp?=	../../image/visp
 
 SYSTEM_SEARCH.visp=\
-	'bin/visp-config:p:% --dumpversion'	\
-	'include/visp/vpConfig.h'		\
-	'lib/libvisp-2.{a,so}'
+  'include{,/visp-*}/visp3/core/vpConfig.h'				\
+  'lib/pkgconfig/visp{,-[0-9]*}.pc:/Version/s/[^.0-9]//gp'		\
+  '{lib/cmake,share,lib}/{VISP,visp}/VISPConfig.cmake'
 
 endif # VISP_DEPEND_MK -----------------------------------------------------
 
